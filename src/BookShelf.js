32982b5a2f4c729d69bef9adb9ebc9eda5cf2537
Omar Monteon <monteon.omar@gmail.com>
2021-03-16 21:44:43 -0600

    feat: Pass onShelfChange handler to BooksGrid.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import BooksGrid from './BooksGrid';
 
-function BookShelf({ title = '', books = [] }) {
+function BookShelf({ title = '', books = [], onShelfChange }) {
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{title}</h2>
-      {/* TODO: New component: BooksGrid */}
+      <BooksGrid books={books} onShelfChange={onShelfChange} />
     </div>
   )
 }
