5afd8ecfdf01148cd0dac7ebd0f46821b69ccffd
Omar Monteon <monteon.omar@gmail.com>
2021-03-16 21:42:56 -0600

    feat: New BooksGrid component.

---
+++ src/BooksGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book from './Book';
+
+function BooksGrid({ books = [], onShelfChange }) {
+  return (
+    <ol className="books-grid">
+      {books.map(book => <li key={book.id}><Book book={book} onShelfChange={onShelfChange} /></li>)}
+    </ol>
+  )
+}
+
+export default BooksGrid;
