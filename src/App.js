5025eff5f7d1b1c1dd0200b3f56d86d1bf2c1588
Omar Monteon <monteon.omar@gmail.com>
2021-03-16 20:30:29 -0600

    feature: Call API on mount to get all books.
    Pass in the response to the BookShelves component as prop.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom';
 import './App.css'
 import BookShelves from './BookShelves';
 import BookSearch from './BookSearch';
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+  componentDidMount() {
+    // TODO: How should errors from the API calls be handled ?
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
   render() {
+    const { books } = this.state;
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <BookShelves />
+          <BookShelves books={books} />
         )} />
-        <Route path='/search' render={({ history }) => (
-          <BookSearch />
-        )} />
+        <Route path='/search' component={BookSearch} />
       </div>
     )
   }
