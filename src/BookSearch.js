dca9a8f8f8930f0bd512482eb0dc871186a78d5d
Omar Monteon <monteon.omar@gmail.com>
2021-03-17 20:05:11 -0600

    feat: Implement BooksAPI search call.
    Passing results to the BookResults component.

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import BookResults from './BookResults'; // Should these imports be default or named ??
 
 class BookSearch extends Component {
+  // Should this state be here or in App.js ??
+  state = {
+    books: [],
+    search: ''
+  }
+  handleSearch = (event) => {
+    // TODO: Implement Debounce
+    const search = event.target.value;
+    this.setState({ search });
+    if (search) {
+      BooksAPI.search(search).then(books => this.setState({ books })); 
+    } else {
+      this.setState({ books: [] });
+    }
+  }
   render() {
+    const { onShelfChange } = this.props;
+    const { books, search } = this.state;
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -11,21 +30,16 @@ class BookSearch extends Component {
             <button className="close-search">Close</button>
           </Link>
           <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" />
-
+            <input 
+              type="text" 
+              placeholder="Search by title or author"
+              value={search}
+              onChange={this.handleSearch}
+               />
+            <BookResults books={books} onShelfChange={onShelfChange} />
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
+        
       </div>
     )
   }
